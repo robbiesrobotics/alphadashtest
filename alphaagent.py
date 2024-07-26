d19bc25ed933bb6639cb1c4dec7ea3cec8951f4e
robbiesrobotics <103398699+robbiesrobotics@users.noreply.github.com>
2024-07-25 20:58:15 -0400

    Update alphaagent.py

--- alphaagent.py
+++ alphaagent.py
@@ -1,7 +1,5 @@
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain.utilities import WolframAlphaAPIWrapper, GoogleSearchAPIWrapper
-from langchain.agents.agent_toolkits import ZapierToolkit
-from langchain.utilities.zapier import ZapierNLAWrapper
 from langchain.agents import Tool, AgentType, initialize_agent
 from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
@@ -12,9 +10,6 @@ import json
 import openai
 
 ########### Setup api Keys ###################################
-ZAPIER_CLIENT_SECRET = st.secrets["ZAPIER_CLIENT_SECRET"]
-ZAPIER_CLIENT_ID = st.secrets["ZAPIER_CLIENT_ID"]
-ZAPIER_NLA_API_KEY = st.secrets["ZAPIER_NLA_API_KEY"]
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
 WOLFRAM_ALPHA_APPID = st.secrets["WOLFRAM_ALPHA_APPID"]
@@ -29,14 +24,10 @@ memory = ConversationBufferMemory(
 
 
 ############ Create LLMs (Language Models) / Agents / Special Tools  #######################
-zapier = ZapierNLAWrapper()
 wolfram = WolframAlphaAPIWrapper()
 search = GoogleSearchAPIWrapper()
-toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
 llm = ChatOpenAI(temperature=0.7,model="gpt-4o", streaming = True)
 llm2 = OpenAI(temperature = 0, streaming= True)
-email_agent = initialize_agent(toolkit.get_tools(), llm2, agent= AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose = True,  handle_parsing_errors = True, memory = memory)
-calendar_agent =  initialize_agent(toolkit.get_tools(), llm, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, verbose = True, handle_parsing_errors = True, memory = memory)
 ###########################################################################################
 
 
